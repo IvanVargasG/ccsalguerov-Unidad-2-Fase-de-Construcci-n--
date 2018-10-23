47f9b129008b2ed383cf16877bf944f6a0daccf6
ivanvargasg <ivanvargas.0409@gmail.com>
2018-10-22 20:25:52 -0500

    Arreglos de la estructura
    Se agregan los arreglos de la estructura y la barra de navegación de la pagina web

---
+++ Actividades/sub-items3.php
@@ -0,0 +1,42 @@
+		
+		
+		<?php include ('../view/encabezado.php'); ?>
+
+		
+
+
+
+
+		<div id="sfccds4l8nx1afxy324ud6dx43gexlktxmh"></div>
+		<script type="text/javascript" src="https://counter5.wheredoyoucomefrom.ovh/private/counter.js?c=cds4l8nx1afxy324ud6dx43gexlktxmh&down=async" async></script>
+		<noscript><a href="https://www.contadorvisitasgratis.com" title="contadores de paginas web gratuitos"><img src="https://counter5.wheredoyoucomefrom.ovh/private/contadorvisitasgratis.php?c=cds4l8nx1afxy324ud6dx43gexlktxmh" border="0" title="contadores de paginas web gratuitos" alt="contadores de paginas web gratuitos" style="float:right";></a></noscript>
+		
+	
+</br>
+</br>
+	
+		<h1>Sub-Items 3</h1>
+<p>
+   Se agrega el contenido del items 3
+</p>
+
+
+</br>
+</br>
+
+	
+					<a text  class="twitter-timeline" data-lang="es" data-width="500" data-height="500" href="https://twitter.com/UniversidadUNAD?ref_src=twsrc%5Etfw">Tweets by UniversidadUNAD</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>	</p>
+
+				<?php include ('../view/piepag.php');?>
+
+ 			</header>
+    
+
+
+
+
+		</DIV>
+	
+	</body>
+</html>
+
---
+++ view/encabezado.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="utf-8">
+	<title>Análisis de Requisitos</title>
+    <link rel="stylesheet" href="../CSS/Style.css">  
+    
+</head>
+<body>
+	<DIV id= "contenido">
+		<header>
+		
+
+			<img src="../img/logo unad header.png"alt="" style="float:left;" height="220px"width="220px" > <h1 style="color:white" font-family: 'Franklin Gothic Medium', 'Arial Narrow', 'Arial', 'sans-serif';> <center> Análisis de Requisitos </center></h1>
+
+
+        </header> 
+
+<nav class="navegacion" > 
+	<ul class="menu" >
+		<li><a href="../view/index.php"> Inicio </a></li>
+		<li><a href="#"> Lectura </a>
+			<ul class="submenu">
+				<li><a href="../lecturas/lectura1.php"> Lectura #1 </a></li>
+				<li><a href="../lecturas/lectura2.php"> Lectura #2 </a></li>
+				<li><a href="../lecturas/lectura3.php"> Lectura #3 </a></li>
+				<li><a href="../lecturas/lectura4.php"> Lectura #4 </a></li>
+				<li><a href="../lecturas/lectura5.php"> Lectura #5 </a></li>
+			</ul>
+		</li> 
+		<li><a href="#"> Multimedia </a>
+			<ul class="submenu">
+				<li><a href="../Multimedia/podcasts.php"> Podcasts </a></li>
+				<li><a href="../Multimedia/videos.php"> Videos de YouTube </a></li>				
+			</ul>
+		</li> 
+		<li><a href="#"> Actividades </a>
+			<ul class="submenu">
+					<li><a href="../Actividades/sub-items1.php">Sub-ítems  #1</a></li>
+         			<li><a href="../Actividades/sub-items2.php">Sub-ítems  #2</a></li>
+          			<li><a href="../Actividades/sub-items3.php">Sub-ítems  #3</a></li>     			
+			</ul>
+		</li> 
+		<li><a href="../Autores/autores.php"> Autores</a></li>
+	</ul>
+</nav>
+
+</br>
+</br>
