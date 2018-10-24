19aa177dd2cb8c95af9251a9eb8a0054870d1853
ccsalguerov <chayanesalguero1@gmail.com>
2018-10-24 18:18:47 -0500

    se agrega la lectura 1
    se agrega la lectura 1 referente al analisis de requisitos desde la ingenieria de software

--- lecturas/lectura1.php
+++ lecturas/lectura1.php
@@ -16,9 +16,40 @@
 </br>
 </br>
 
-		<h1>Lectura 1</h1>
+		<h1>Análisis de requisitos
+ 
+
+</h1>
 <p>
-    Se describe la primera lectura
+    La ingeniería de requisitos es un proceso donde se refinan en detalle los requisitos de un sistema y el papel asignado al software. Este proceso tiene unos actores claves que son el desarrollador y el cliente las interacciones entre estos dos se denominan análisis “el cliente intenta replantear un sistema confuso, a nivel de descripción de datos, funciones y comportamientos, en detalles concretos. El desarrollador actúa como interrogador, como consultor, como persona que resuelve problemas y como negociador” (pressman, 2010)
+Aunque a primera vista parezca algo sencillo la comunicación entre ambos actores es muy densa, siempre hay ocasiones para malas interpretaciones o falta de información.
+El análisis de requisitos permite al ingeniero de software especificar características operacionales del software además establece las restricciones que debe cumplir el software.
+El análisis de requisitos del software se puede subdividir en cinco áreas de esfuerzo:
+1.      Reconocimiento del problema<br>
+2.      Evaluación y síntesis<br>
+3.      Modelado<br>
+4.      Especificación<br>
+5.      Revisión <br>
+(pressman, 2010)<br>
+Todos los métodos de análisis se relacionan por un conjunto de principios operativos:
+1.      Debe representarse y entenderse el dominio de la información de un problema.<br>
+2.      Deben definirse las funciones que debe realizar el software<br>
+3.      Debe representarse el comportamiento del software (como consecuencia de acontecimientos externos),<br>
+4.      Deben dividirse los modelos que representan información, función y comportamiento de manera que se descubran los detalles por capas (o jerárquicamente).<br>
+5.      El proceso de análisis debería ir desde la información esencial hasta el detalle de la implementación.<br>
+(pressman, 2010)<br>
+<picture><img src="../img/lectura1.png"></picture><br>
+
+<a href="https://goo.gl/images/6dxozQ">Imagen Recuperada de</a><br>
+
+
+Además de los principios operativos mencionados anteriormente, se sugiere un conjunto de principios directrices para la ingeniería de requerimientos:<br>
+1.      Entender el problema antes de empezar a crear el modelo de análisis.<br>
+2.      Desarrollar prototipos que permitan al usuario entender cómo será la interacción hombre-máquina.<br>
+3.      Registrar el orden y la razón de cada requerimiento,<br>
+4.      Usar múltiples planteamientos de requerimientos.<br>
+5.      Priorizar los requerimientos.<br>
+6.      Trabajar para eliminar la ambigüedad.<br>
 </p>
 
 
