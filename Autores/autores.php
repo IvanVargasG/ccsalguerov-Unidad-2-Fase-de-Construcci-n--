58b371d7b5d732cdc9608754ff7aa7a95c160e6a
ccsalguerov <chayanesalguero1@gmail.com>
2018-10-26 21:57:48 -0500

    Autores
    se agrega mi minibiografia mas foto

--- Autores/autores.php
+++ Autores/autores.php
@@ -5,7 +5,13 @@
 
 <h1>Autores</h1>
 <p>
-Se agrega los autores del OVI
+Cristian Chayane Salguero Valero
+<img src="../img/fotocristian.png" height="120" width="120"align="left"><br>
+<br>
+Mi nombre  es Cristian Chayane Salguero Valero naci en el municipio de Choconta el 24 de enero de 1998, soy estudiante <br>
+de ingeniería de sistemas en la Universidad Nacional Abierta y a Distancia, me gradue como bachiller en la Escuela Normal<br>
+Superior Maria Auxiliadora de Villapinzon, me gusta programar, el rock y los videojuegos vivo en Tunja actualmente, no<br>
+tengo hijos ni esposa.
 </p>
 
 
