0b64c02ea43f3ca8f24b9da8e153fa9d02fff611
ccsalguerov <chayanesalguero1@gmail.com>
2018-10-27 17:58:58 -0500

    Ajuste de autor
    ajuste a autores

--- Autores/autores.php
+++ Autores/autores.php
@@ -8,10 +8,14 @@
 Cristian Chayane Salguero Valero
 <img src="../img/fotocristian.png" height="120" width="120"align="left"><br>
 <br>
-Mi nombre  es Cristian Chayane Salguero Valero naci en el municipio de Choconta el 24 de enero de 1998, soy estudiante <br>
+Mi nombre  es Cristian Chayane Salguero Valero identificado con la cedula de ciudadania  1049652609, naci <br>
+en el municipio de Choconta el 24 de enero de 1998, soy estudiante <br>
 de ingeniería de sistemas en la Universidad Nacional Abierta y a Distancia, me gradue como bachiller en la Escuela Normal<br>
-Superior Maria Auxiliadora de Villapinzon, me gusta programar, el rock y los videojuegos vivo en Tunja actualmente, no<br>
-tengo hijos ni esposa.
+Superior Maria Auxiliadora de Villapinzon, me gusta programar, el rock y los videojuegos vivo en Tunja actualmente<br>
+
+Mi Skype :salguerocristian1@hotmail.com<br>
+Mi Email :chayanesalguero1@gmail.com
+
 </p>
 
 
